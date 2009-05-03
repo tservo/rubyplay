d85e2d423286bd5f341d62abaf336639eed98242
Barce <jbarcelona@gmail.com>
2009-05-03 13:22:47 -0700

    done with genpass

--- genpass.rb
+++ genpass.rb
@@ -1,4 +1,4 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
 #
 #
 # this program generates
@@ -6,4 +6,27 @@
 # based on /usr/share/dict/words
 # 
 
+DICT = '/usr/share/dict/words'
+
+words = []
+
+File.open(DICT, 'r') do |aFile|
+	aFile.each_line { |line| words.push(line.chomp) }
+end
+
+puts words.size.to_s
+first = 2344
+while (words[first].to_s.length > 6) 
+	first = rand(words.size)
+	puts words[first].to_s
+end 
+
+second = 2344
+while (words[second].to_s.length > 6) 
+	second = rand(words.size)
+	puts words[second].to_s
+end 
+num    = rand(99)
+
+puts words[first] + num.to_s + words[second]
 
